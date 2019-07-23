56364188d237491d56a047161bc2045a0cf84828
Kaan Yagci <kaan.yagci@clique.paris>
2019-07-23 03:41:22 +0200

    [bixby-cli] - ErrorCode class implemented and used in CustomError class

---
+++ src/structs/ErrorCode.enum.ts
@@ -0,0 +1,11 @@
+export enum ErrorCode {
+    connectionError = 404,
+    versionError = 5000,
+    invalidPatchVersion = 5001,
+    invalidMinorVersion = 5002,
+    invalidMajorVersion = 5003,
+    invalidVersionNumber = 5010,
+    invalidPermissionValue = 10001,
+    capsuleBxbContent = 9000,
+    invalidFormatValue = 9001,
+}
